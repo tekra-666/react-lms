2bedac09d5c889b1fcb02e362b3856bfc972caf3
tekra-666 <shivinboxmail@gmail.com>
2022-11-20 12:31:06 +0530

    modified:   src/Pages/Candidate/ProfileCandidate.js

--- src/Pages/Candidate/ProfileCandidate.js
+++ src/Pages/Candidate/ProfileCandidate.js
@@ -21,11 +21,27 @@ const ProfileCandidate = () => {
         </div>
       </div>
 
-      <div className="card-info-education card-style">
+      <div className="card-style card">
         <div className="card-title">Educational Information</div>
-        <div>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        <div className="card-body">
+          <div className="card-body info-education">
+            <div>Course</div>
+            <div>Percentage</div>
+            <div>Grade</div>
+            <div>College Name</div>
+            <div>University Name</div>
+            <div>Passing Year</div>
+          </div>
+          <div className="card-body info-education">
+            <div>B.Tech</div>
+            <div>80%</div>
+            <div>A</div>
+            <div>ITS College of Engineering</div>
+            <div>Uttar Pradesh Technical University (UPTU)</div>
+            <div>Passing Year</div>
+          </div>
         </div>
+        
       </div>
       
       <div>Professional Information</div>
